32fc0fada0db548e57d07e5857e32542f646eed3
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-10-09 16:06:08 -0300

    Projeto inciado

---
+++ classificaAudio.m
@@ -0,0 +1,15 @@
+% Autor: Denilson Gomes Vaz da Silva
+% Departamento de Engenharia de Computação
+% Processamento Digital de Sinais
+% Script para classificar
+
+[n1,Fs1] = audioread('nao (1).wav');
+[n2,Fs2] = audioread('nao (2).wav');
+[n3,Fs3] = audioread('nao (3).wav');
+[n4,Fs4] = audioread('nao (4).wav');
+[n5,Fs5] = audioread('nao (5).wav');
+[s1,Fs1] = audioread('sim (1).wav');
+[s2,Fs2] = audioread('sim (2).wav');
+[s3,Fs3] = audioread('sim (3).wav');
+[s4,Fs4] = audioread('sim (4).wav');
+[s5,Fs5] = audioread('sim (5).wav');
